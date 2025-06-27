0296f5db2c50c66315a8ee4681be3b2786848ea7
mur073 <murodzon104@gmail.com>
2025-06-28 01:15:56 +0500

    meow x2

--- main.cpp
+++ main.cpp
@@ -509,7 +509,7 @@ int main() {
 #ifdef _WIN32
     std::string port = "COM1";
 #else
-    std::string port = "/dev/ttys004";
+    std::string port = "/dev/ttyS4";
 #endif
 
     MKR5Controller controller(port);
